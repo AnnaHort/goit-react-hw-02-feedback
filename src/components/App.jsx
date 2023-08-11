6e8767bc4fde4e90ecba17efd06ea78d268051d9
AnnaHort <annahort3d@gmail.com>
2023-08-11 20:01:12 +0300

    update homeWork #1 part

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-import { Container, Button, List} from './Statistic.styled';
+import { Container } from './Statistic.styled';
+import { Statistics } from './Statistics';
+import { FeedbackOptions } from './FeedbackOptions';
+import Section from './Section';
 
 export class App extends Component {
   state = {
@@ -22,7 +25,7 @@ export class App extends Component {
         neutral: prevState.neutral + 1,
       };
     });
-  }
+  };
 
   handleClickBad = () => {
     this.setState(prevState => {
@@ -30,42 +33,68 @@ export class App extends Component {
         bad: prevState.bad + 1,
       };
     });
-  }
+  };
 
   countTotalFeedback = () => {
     const { good, neutral, bad } = this.state;
     const allFeedback = good + neutral + bad;
-    return allFeedback
-  }
+    return allFeedback;
+  };
 
   countPositiveFeedbackPercentage = () => {
     const { good, neutral, bad } = this.state;
     const positiveFeedback = good;
     const totalFeedback = good + neutral + bad;
-  
+
     if (totalFeedback === 0) {
-      return 0; 
+      return 0;
     }
-  
+
     const positivePercentage = (positiveFeedback / totalFeedback) * 100;
     return Math.round(positivePercentage);
-  }
+  };
+
+  handleLeaveFeedback = option => {
+    if (option === 'good') {
+      this.handleClickGood();
+    } else if (option === 'neutral') {
+      this.handleClickNeutral();
+    } else if (option === 'bad') {
+      this.handleClickBad();
+    }
+  };
 
   render() {
+    const { good, neutral, bad } = this.state;
+    const total = this.countTotalFeedback();
+    const positivePercentage = this.countPositiveFeedbackPercentage();
+    const hasFeedback = good + neutral + bad > 0;
+
     return (
       <Container>
-        <h2>Please leave feedback</h2>
-        <List>
-          <li><Button onClick={this.handleClickGood}>Good</Button></li>
-          <li><Button onClick={this.handleClickNeutral}>Neutral</Button></li>
-          <li><Button onClick={this.handleClickBad}>Bad</Button></li>
-        </List>
-        <h2>Statistics</h2>
-        <p>Good: {this.state.good}</p>
-        <p>Neutral: {this.state.neutral}</p>
-        <p>Bad: {this.state.bad}</p>
-        <p>Total: {this.countTotalFeedback()}</p>
-        <p>Positive feedback: {this.countPositiveFeedbackPercentage()}%</p>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={['good', 'neutral', 'bad']}
+            onLeaveFeedback={this.handleLeaveFeedback}
+            onLeaveFeedbackGood={this.handleClickGood}
+            onLeaveFeedbackNeutral={this.handleClickNeutral}
+            onLeaveFeedbackBad={this.handleClickBad}
+          />
+        </Section>
+
+        {/* блок статистики */}
+        {hasFeedback && (
+          <Section title="Statistics">
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={total}
+              positivePercentage={positivePercentage}
+            />
+          </Section>
+        )}
+
       </Container>
     );
   }
